98d13e3c45da632fd62abea4c762ef79661d7df9
will <will.santos97@hotmail.com>
2023-05-24 10:01:32 -0300

    second commit

--- src/app/modules/home/components/todo-input-add-item/todo-input-add-item.component.ts
+++ src/app/modules/home/components/todo-input-add-item/todo-input-add-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-item',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-input-add-item.component.scss']
 })
 export class TodoInputAddItemComponent implements OnInit {
-
+  @Output("emitTask") public emitTask: EventEmitter<string> = new EventEmitter();
+  public task: string = "";
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public submitItem(): void {
+    this.emitTask.emit(this.task);
+    this.task = "";
+  }
+
+  
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+//interface
+import { TaskList } from '../../models/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-
+  public taskList: Array<TaskList> = new Array();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public addTask(e: string): void {
+    console.log("task: ", e);
+    this.taskList.push({
+      task: e, checked: false
+    });
+  }
+
+  public deleteItem(i: number): void {
+    this.taskList.splice(i, 1);
+  }
+
+  public deleteAll(): void {
+    const confirm = window.confirm("Você deseja deletar tudo ?");
+    if (confirm) this.taskList = [];
+    
   }
 
 }
